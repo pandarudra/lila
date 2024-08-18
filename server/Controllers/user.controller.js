c701fa064fb40fbb32cb4a493cb07ea1578d852d
pandarudra <rudrapanda8206@gmail.com>
2024-08-19 00:29:32 +0530

    bcryptjs up-1

--- server/Controllers/user.controller.js
+++ server/Controllers/user.controller.js
@@ -1,9 +1,11 @@
 import User from "../Models/user.model.js";
+import { hash } from "../Utils/hash.js";
 
 const signup = async (req, res) => {
   const { email, username, password } = req.body;
   try {
-    const user = new User({ email, username, password });
+    const hashedPassword = await hash(password);
+    const user = new User({ email, username, password: hashedPassword });
     await user.save();
     res.status(201).json({ user });
   } catch (error) {

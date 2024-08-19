3e1454f0d2f96778da0682a4d5886b8b8d3c0552
parambehera <paramjyotibehera2005@gamil.com>
2024-08-19 09:16:37 +0530

    Clent up-4

--- client/src/renderer/src/components/ForgotPassword.jsx
+++ client/src/renderer/src/components/ForgotPassword.jsx
@@ -6,21 +6,21 @@ const ForgotPassword = () => {
       <h1 className="text-4xl text-center font-mono ">Change Your Password</h1>
       <form className="flex flex-col gap-3 items-center">
         <input
-          type="password"
-          placeholder="New Password"
+          type="email"
+          placeholder="Email"
           className="w-1/2 p-2 m-2 border border-gray-900 rounded"
         />
-        <input
+        {/* <input
           type="password"
           placeholder="Confirm Password"
           className="w-1/2 p-2 m-2 border border-gray-900 rounded"
-        />
+        /> */}
        
         <button
           type="submit"
           className="w-1/2 p-2 m-2 hover:bg-cyan-950 bg-cyan-400 text-white rounded"
         >
-          CHANGE PASSWORD
+        SUMBIT
         </button>
       </form>
       <div className="flex justify-center items-center">
